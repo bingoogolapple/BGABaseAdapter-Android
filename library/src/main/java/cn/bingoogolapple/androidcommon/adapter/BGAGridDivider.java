00a2602439a7d7162846f7149b7f3b26c50e2576
bingoogolapple <bingoogolapple@gmail.com>
2017-01-09 23:21:33 +0800

    「add」BGAGridDivider

---
+++ library/src/main/java/cn/bingoogolapple/androidcommon/adapter/BGAGridDivider.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2016 bingoogolapple
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.bingoogolapple.androidcommon.adapter;
+
+import android.graphics.Rect;
+import android.support.annotation.DimenRes;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:17/1/9 下午11:12
+ * 描述:
+ */
+public class BGAGridDivider extends RecyclerView.ItemDecoration {
+    private int mSpace;
+
+    private BGAGridDivider(int space) {
+        mSpace = space;
+    }
+
+    /**
+     * 设置间距资源 id
+     *
+     * @param resId
+     * @return
+     */
+    public static BGAGridDivider newInstanceWithSpaceRes(@DimenRes int resId) {
+        return new BGAGridDivider(BGAAdapterUtil.getDimensionPixelOffset(resId));
+    }
+
+    /**
+     * 设置间距
+     *
+     * @param spaceDp 单位为 dp
+     * @return
+     */
+    public BGAGridDivider newInstanceWithSpaceDp(int spaceDp) {
+        return new BGAGridDivider(BGAAdapterUtil.dp2px(spaceDp));
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        outRect.left = mSpace;
+        outRect.right = mSpace;
+        outRect.top = mSpace;
+        outRect.bottom = mSpace;
+    }
+}
